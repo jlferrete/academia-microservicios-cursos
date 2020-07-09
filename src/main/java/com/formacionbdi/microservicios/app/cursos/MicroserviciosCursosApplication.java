fbe4af60e59919a67f2801df0449f80830d422e1
JlFerrete <joseluisferrete@gmail.com>
2020-07-09 12:37:06 +0200

    Creating commons library to reuse entity Alumno

--- src/main/java/com/formacionbdi/microservicios/app/cursos/MicroserviciosCursosApplication.java
+++ src/main/java/com/formacionbdi/microservicios/app/cursos/MicroserviciosCursosApplication.java
@@ -2,10 +2,13 @@ package com.formacionbdi.microservicios.app.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.formacionbdi.microservicios.commons.alumnos.models.entity",
+			 "com.formacionbdi.microservicios.app.cursos.models.entity"})
 public class MicroserviciosCursosApplication {
 
 	public static void main(String[] args) {
