8a700828bed677219cb9381798106febaba9f3dd
JlFerrete <joseluisferrete@gmail.com>
2020-07-09 12:58:35 +0200

    Adding ongoing relationship with your students

--- src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
+++ src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
@@ -1,16 +1,22 @@
 package com.formacionbdi.microservicios.app.cursos.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 
 @Entity
 @Table(name="cursos")
@@ -22,13 +28,22 @@ public class Curso {
 	
 	private String nombre;
 	
-		@Column(name="created_at")
+	@Column(name="created_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createdAt;
-		
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Alumno> alumnos;
+			
 	@PrePersist
 	public void prePersist() {
 		this.createdAt = new Date();
+	}
+	
+	
+
+	public Curso() {
+		this.alumnos = new ArrayList<>();
 	}
 
 	public Long getId() {
@@ -54,6 +69,21 @@ public class Curso {
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
+
+	public List<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(List<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
 	
+	public void addAlumno(Alumno alumno) {
+		this.alumnos.add(alumno);
+	}
+	
+	public void removeAlumno(Alumno alumno) {
+		this.alumnos.remove(alumno);
+	}
 	
 }
