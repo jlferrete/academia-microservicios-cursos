6536d768061645cfeb9f98fa00dd2c0d4139299c
JlFerrete <joseluisferrete@gmail.com>
2020-07-09 11:18:16 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/microservicios/app/cursos/MicroserviciosCursosApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.app.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosCursosApplication.class, args);
+	}
+
+}
