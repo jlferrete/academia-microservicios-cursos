3beb19cdbb6dcfa2a43ef9fcaa831604233025ab
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 12:34:12 +0200

    Adding validation rules in the entities

--- src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
@@ -3,8 +3,11 @@ package com.formacionbdi.microservicios.app.cursos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,84 +24,89 @@ import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 public class CursoController extends CommonController<Curso, CursoService> {
 
 	@PutMapping("/{id}")
-	public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id) {
+	public ResponseEntity<?> editar(@Valid @RequestBody Curso curso, BindingResult result, @PathVariable Long id) {
+
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
+
 		Optional<Curso> o = this.service.findById(id);
-		if(!o.isPresent()) {
+		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		Curso dbCurso = o.get();
 		dbCurso.setNombre(curso.getNombre());
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));		
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
 	}
-	
+
 	@PutMapping("/{id}/asignar-alumnos")
 	public ResponseEntity<?> asignarAlumnos(@RequestBody List<Alumno> alumnos, @PathVariable Long id) {
 		Optional<Curso> o = this.service.findById(id);
-		if(!o.isPresent()) {
+		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
 		Curso dbCurso = o.get();
-		
+
 		alumnos.forEach(a -> {
 			dbCurso.addAlumno(a);
 		});
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));	
-		
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+
 	}
-	
+
 	@PutMapping("/{id}/eliminar-alumno")
 	public ResponseEntity<?> eliminarAlumno(@RequestBody Alumno alumno, @PathVariable Long id) {
 		Optional<Curso> o = this.service.findById(id);
-		if(!o.isPresent()) {
+		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
 		Curso dbCurso = o.get();
-			
+
 		dbCurso.removeAlumno(alumno);
-				
-		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));	
-		
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+
 	}
-	
+
 	@GetMapping("alumno/{id}")
 	public ResponseEntity<?> buscarPorAlumnoId(@PathVariable Long id) {
-		
+
 		Curso curso = service.findCursoByAlumnoId(id);
-		
+
 		return ResponseEntity.ok(curso);
 	}
-	
+
 	@PutMapping("/{id}/asignar-examenes")
 	public ResponseEntity<?> asignarExamenes(@RequestBody List<Examen> examenes, @PathVariable Long id) {
 		Optional<Curso> o = this.service.findById(id);
-		if(!o.isPresent()) {
+		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
 		Curso dbCurso = o.get();
-		
+
 		examenes.forEach(e -> {
 			dbCurso.addExamen(e);
 		});
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));	
-		
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+
 	}
-	
+
 	@PutMapping("/{id}/eliminar-examen")
 	public ResponseEntity<?> eliminarExamen(@RequestBody Examen examen, @PathVariable Long id) {
 		Optional<Curso> o = this.service.findById(id);
-		if(!o.isPresent()) {
+		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
 		Curso dbCurso = o.get();
-			
+
 		dbCurso.removeExamen(examen);
-				
-		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));	
-		
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
+
 	}
-	
+
 }
--- src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
+++ src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
@@ -16,6 +16,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
 
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
@@ -28,6 +29,7 @@ public class Curso {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotEmpty
 	private String nombre;
 	
 	@Column(name="created_at")
