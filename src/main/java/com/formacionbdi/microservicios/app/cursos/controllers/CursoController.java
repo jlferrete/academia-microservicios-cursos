0e45146a2d28259d8cc664be1b662819707eda38
JlFerrete <joseluisferrete@gmail.com>
2020-07-14 13:17:48 +0200

    Testing Spring Cloud Load Balancer

--- src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/cursos/controllers/CursoController.java
@@ -1,11 +1,14 @@
 package com.formacionbdi.microservicios.app.cursos.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -24,6 +27,17 @@ import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 @RestController
 public class CursoController extends CommonController<Curso, CursoService> {
 
+	@Value("${config.balanceador.test}")
+	private String balanceadorTest;
+	
+	@GetMapping("/balanceador-test")
+	public ResponseEntity<?> balanceadorTest() {
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("balanceador", balanceadorTest);
+		response.put("cursos", service.findAll());
+		return ResponseEntity.ok(response);
+	}
+	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@Valid @RequestBody Curso curso, BindingResult result, @PathVariable Long id) {
 
@@ -123,5 +137,9 @@ public class CursoController extends CommonController<Curso, CursoService> {
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(dbCurso));
 
 	}
+
+
+	
+	
 
 }
